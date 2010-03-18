57c230e26f381d3592b7e60d6025202c0264b892
unknown <yuanh@.(none)>
2010-03-18 13:50:36 +0100

    app files

---
+++ zend_prototyp/warenkorb/public/js/warenkorb.js
@@ -0,0 +1,15 @@
+<SCRIPT language=javascript>
+					var sel;
+	
+					function doMouseout()
+{
+       sel.style.visibility="hidden";
+}
+
+function doMouseover(obj)
+{
+       sel=obj;
+       sel.style.visibility="visible";
+}
+          
+ </SCRIPT>
